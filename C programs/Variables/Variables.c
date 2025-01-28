bf2f9daa43986d79266e37243c054f43f4f52f3d
POOVARASAN G <165827909+cadburyboy21@users.noreply.github.com>
2025-01-28 12:58:07 +0530

    variables

---
+++ C programs/Variables/Variables.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char characterName[]= "Poovarasan";
+    int characterAge=20;
+    printf("My Name is %s \n",characterName);
+    printf("My age is %d",characterAge);
+
+    }
