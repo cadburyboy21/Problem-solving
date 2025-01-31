3b67136624634fe177dd842408717d23839ac86d
POOVARASAN G <165827909+cadburyboy21@users.noreply.github.com>
2025-01-31 11:14:39 +0530

    Numbers

---
+++ C programs/Numbers/Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int num=6;
+    printf("%f\n",5+4.5);
+    printf("%f\n",5/1);
+    printf("%d\n",num);
+    printf("%f\n", sqrt(36));
+    printf("%f\n", pow(2,3));
+    printf("%f\n", ceil(35.356));
+    printf("%f", floor(35.567));
+    return 0;
+}
