c8d58489cecd508fa801f7713c748e708c9ad098
POOVARASAN G <165827909+cadburyboy21@users.noreply.github.com>
2025-02-04 18:56:34 +0530

    C program

---
+++ C programs/Adding Two numbers/Adding2numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int Num1;
+    int Num2;
+    printf("Enter the Number1:");
+    scanf("%d",&Num1);
+    printf("Enter the Number2:");
+    scanf("%d",&Num2);
+
+    printf(" Answer is %d",Num1+Num2);
+    return 0;
+}
