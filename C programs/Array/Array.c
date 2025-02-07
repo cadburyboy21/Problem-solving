d721f9dd6aebbbb3d5b9bb6f63d1ea340b53d38d
POOVARASAN G <165827909+cadburyboy21@users.noreply.github.com>
2025-02-07 20:41:57 +0530

    Array

---
+++ C programs/Array/Array.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    int Arrays[] = { 2 , 4 ,6,8,10};
+    printf("The Index value of 1 is %d",Arrays[1]);
+}
---
+++ C programs/Array/README.md
@@ -0,0 +1,7 @@
+ ARRAY [ 2,4,6,8,10]
+
+INDEX OF 1?
+
+OUTPUT
+
+The Index value of 1 is 4
