7fb2d1be97aada4a302ae7c8fd908d50c78ee10c
POOVARASAN G <165827909+cadburyboy21@users.noreply.github.com>
2025-02-08 15:12:55 +0530

    Function

---
+++ C programs/Functions/Example 1.c
@@ -0,0 +1,14 @@
+//Example 1: Function without Parameters and Return Value
+
+#include<stdio.h>
+
+void Greet()
+{
+    printf("Hello C programming!!");
+}
+
+int main()
+{
+    Greet();
+    return 0;
+}
---
+++ C programs/Functions/Example 2.c
@@ -0,0 +1,13 @@
+//Example 2: Function with Parameters and Return Value
+
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int sum = add(5, 10);
+    printf("Sum: %d\n", sum);
+    return 0;
+}
