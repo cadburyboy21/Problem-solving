6f95678d8ab317d9931b5de2371d3e8ebb18d1dd
POOVARASAN G <165827909+cadburyboy21@users.noreply.github.com>
2025-04-18 11:39:08 +0530

    Java

---
+++ Java program/AreaofRectangle.java
@@ -0,0 +1,12 @@
+public class AreaofRectangle {
+   public static void main(String[] args){
+    int Length=45;
+    int width=20;
+    int Area= Length*width;
+    System.out.println("The Area of Rectangle is "+ Area);
+   }
+}
+
+// output:
+
+// The Area of Rectangle is 900
---
+++ Java program/AssignmentOperator.java
@@ -0,0 +1,23 @@
+public class AssignmentOperator {
+    public static void main(String[] args){
+        int Num=10;
+        
+        // Addition
+        Num +=5;
+
+        // Subraction
+        Num -=5;
+
+        // Multiplication
+        Num *=5;
+
+        //Division
+        Num /=5;
+
+        //To find the Reminder
+        Num %=5;
+
+        System.out.println(Num);
+    
+    }
+}
