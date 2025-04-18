bd413223f133a315269b09ebfdef7adf1fb889f7
POOVARASAN G <165827909+cadburyboy21@users.noreply.github.com>
2025-04-18 22:24:50 +0530

    Create AddDatainArray.java

---
+++ Java program/Array/AddDatainArray.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.Scanner;
+
+public class AddDatainArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("You entered:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+
+        sc.close();
+    }
+}
+
+
+// output:
+
+// Enter array size: 5
+// Enter 5 elements:
+// 1
+// 2
+// 3
+// 4
+// 5
+// You entered:
+// 1
+// 2
+// 3
+// 4
+// 5
