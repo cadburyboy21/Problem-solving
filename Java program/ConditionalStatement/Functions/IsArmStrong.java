de4be93e48b3e5e51b0156d4c94993eac172d9dc
POOVARASAN G <165827909+cadburyboy21@users.noreply.github.com>
2025-05-31 12:06:39 +0530

    Array

--- Java program/ConditionalStatement/Functions/IsArmStrong.java
+++ Java program/ConditionalStatement/Functions/IsArmStrong.java
@@ -4,38 +4,39 @@ import java.util.Scanner;
 
 public class IsArmStrong {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        int n =input.nextInt();
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
         System.out.println(isArmstrong(n));
 
-        for(int i=100;i<1000;i++){
-            if( isArmstrong(i)){
+        for (int i = 100; i < 1000; i++) {
+            if (isArmstrong(i)) {
                 System.out.println(i + " ");
             }
         }
-        
+
     }
-    static boolean isArmstrong(int n){
+
+    static boolean isArmstrong(int n) {
         int original = n;
-        int sum=0;
-        while(n>0){
-        int rem=n%10;
-        n= n/10;
-        sum=sum+rem*rem*rem;
+        int sum = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem * rem * rem;
         }
-        if(sum==original){
+        if (sum == original) {
             return true;
         }
         return false;
     }
-    
+
 }
 
 // output
 
 // 153 <-input
 // true
-// 153 
+// 153
 // 370
 // 371
 // 407
